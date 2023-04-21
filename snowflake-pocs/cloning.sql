65f7e82fd920eeb3b7660fc5f800c5f32126b786
kannankandasamy <kannanvijay@hotmail.com>
2023-04-21 12:53:12 +0530

    added load files

---
+++ snowflake-pocs/cloning.sql
@@ -0,0 +1,53 @@
+/*
+ * Script 	:	POC for Zero copy cloning on snowflake
+ * Author	:	Kannan Kandasamy
+ */
+
+USE DATABASE PERM_DB;
+USE WAREHOUSE PROCESS_WH;
+USE SCHEMA MARKETING;
+
+/*
+ * cloned table will not affect original table
+ * 
+ * swap with other tables
+ * 
+ */
+
+CREATE OR REPLACE TABLE CUSTOMER_DEMO2 (
+	CUST_ID 		INT,
+	CUST_NAME		VARCHAR(100),
+	ENROLL_DATE		DATE,
+	CUST_ADDRESS	VARCHAR(500)
+);
+
+
+INSERT INTO CUSTOMER_DEMO2
+SELECT 1, 'Kohli', '2000-03-05','Chennai' UNION ALL
+SELECT 2, 'Sachin', '2000-03-05','Mumbai' UNION ALL
+SELECT 3, 'Rahul', '2000-02-05','Hydrabad'  
+;
+
+CREATE TABLE CUSTOMER_CLONE CLONE CUSTOMER_DEMO2;
+
+SHOW TABLES;
+
+CREATE DATABASE PERM_CONE CLONE PERM_DB;
+
+SHOW DATABASES;
+
+USE DATABASE PERM_CONE;
+
+SHOW SCHEMAS;
+
+USE SCHEMA marketing;
+
+SHOW TABLES;
+
+SELECT * FROM INFORMATION_SCHEMA.TABLE_STORAGE_METRICS;
+
+SELECT * FROM customer_demo;
+
+SELECT * FROM customer_tt_demo;
+
+ALTER TABLE customer_demo SWAP WITH customer_tt_demo;
